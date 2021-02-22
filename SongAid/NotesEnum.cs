586f1fd0af045b7e9247f670f93b7df5889d4733
Ethan <ethanharman12@gmail.com>
2021-02-22 12:26:09 -0500

    Arranged Notes in Keys

--- SongAid/NotesEnum.cs
+++ SongAid/NotesEnum.cs
@@ -6,26 +6,25 @@ using System.Threading.Tasks;
 namespace SongAid
 {
     public enum Note
-    {
-        
-        Af,
-        A,
-        As,
-        Bf,
-        B,
-        Bs,
-        Cf,
-        C,
-        Cs,
-        Df,
-        D,
-        Ds,
-        Ef,
-        E,
-        F,
-        Fs,
-        Gf,
-        G,
-        Gs
+    {        
+        Af = 91,
+        A = 100,
+        As = 110,
+        Bf = 111,
+        B = 120,
+        Bs = 130,
+        Cf = 131,
+        C = 10,
+        Cs = 20,
+        Df = 21,
+        D = 30,
+        Ds = 40,
+        Ef = 41,
+        E = 50,
+        F = 60,
+        Fs = 70,
+        Gf = 71,
+        G = 80,
+        Gs = 90
     }
 }
